bbfbcce401aa47499602a2dd225f7e4fcc4419ac
SaraQian <abigail830@163.com>
2018-12-22 16:02:03 +0800

    fine tune for contract flow

--- src/test/java/com/github/abigail830/ContractProvider/controller/HealthMonitorContractBaseController.java
+++ src/test/java/com/github/abigail830/ContractProvider/controller/HealthMonitorContractBaseController.java
@@ -1,18 +1,17 @@
 package com.github.abigail830.ContractProvider.controller;
 
 import io.restassured.module.mockmvc.RestAssuredMockMvc;
+import org.junit.Before;
 import org.junit.Ignore;
-import org.junit.jupiter.api.BeforeEach;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 
-import static org.junit.jupiter.api.Assertions.*;
 
 @Ignore
 @AutoConfigureMockMvc
-class HealthMonitorControllerTest {
+public class HealthMonitorContractBaseController {
 
-    @BeforeEach
-    void setUp() {
+    @Before
+    public void setUp() {
         RestAssuredMockMvc.standaloneSetup(new HealthMonitorController());
     }
 }
